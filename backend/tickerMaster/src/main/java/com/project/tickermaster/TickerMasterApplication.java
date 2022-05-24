35545616898e2ffbfe0cb9ca66c7b6003ed423d2
Linqiang Guo <105519688+pockyitachi@users.noreply.github.com>
2022-05-24 18:00:22 -0400

    Initialization

---
+++ backend/tickerMaster/src/main/java/com/project/tickermaster/TickerMasterApplication.java
@@ -0,0 +1,13 @@
+package com.project.tickermaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickerMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TickerMasterApplication.class, args);
+    }
+
+}
