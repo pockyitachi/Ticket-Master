80266d2f5d20791de60ecdd26c10aea938e34eb6
Linqiang Guo <105519688+pockyitachi@users.noreply.github.com>
2022-05-28 17:34:27 -0400

    Initial Test

---
+++ backend/demo1/src/main/java/com/project/demo1/Demo1Application.java
@@ -0,0 +1,16 @@
+package com.project.demo1;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.project.demo1.mapper")
+public class Demo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1Application.class, args);
+	}
+
+}
